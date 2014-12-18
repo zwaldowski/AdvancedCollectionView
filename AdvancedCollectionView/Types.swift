008e765524bf3de002d04b09f9f26cca8ee0a1af
Zachary Waldowski <zach@waldowski.me>
2014-12-18 03:00:05 -0500

    "Take n at a time" function

--- AdvancedCollectionView/Types.swift
+++ AdvancedCollectionView/Types.swift
@@ -17,3 +17,15 @@ public enum SectionOperationDirection {
 @transparent func contains<T where T: RawOptionSetType, T: NilLiteralConvertible>(mask: T, bit: T) -> Bool {
     return (mask & bit) != nil
 }
+
+// MARK: Utilities
+
+func take<C: Sliceable where C.Index: Strideable>(collection: C, eachSlice slice: C.Index.Stride) -> LazySequence<MapSequenceView<StrideTo<C.Index>, C.SubSlice>> {
+    let start = collection.startIndex
+    let end = collection.endIndex
+    return lazy(stride(from: start, to: end, by: slice)).map { startIndex in
+        let length = min(slice, startIndex.distanceTo(end))
+        let range = startIndex..<startIndex.advancedBy(length)
+        return collection[range]
+    }
+}
