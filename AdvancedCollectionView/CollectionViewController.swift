a23be60cef30fde05b2ac2c20b24dcafa75e9236
Zachary Waldowski <zach@waldowski.me>
2014-12-29 00:26:51 -0500

    Port collection view controller

---
+++ AdvancedCollectionView/CollectionViewController.swift
@@ -0,0 +1,116 @@
+//
+//  CollectionViewController.swift
+//  AdvancedCollectionView
+//
+//  Created by Zachary Waldowski on 12/28/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+import UIKit
+
+public class CollectionViewController: UICollectionViewController, DataSourceContainer {
+    
+    private var dataSourceContext = 0
+    private let updateDebugging = false
+    
+    deinit {
+        collectionView?.removeObserver(self, forKeyPath: "dataSource", context: &dataSourceContext)
+    }
+    
+    public override func loadView() {
+        super.loadView()
+        
+        //  We need to know when the data source changes on the collection view so we can become the delegate for any data source subclasses.
+        collectionView?.addObserver(self, forKeyPath: "dataSource", options: NSKeyValueObservingOptions.Initial | NSKeyValueObservingOptions.New, context: &dataSourceContext)
+    }
+    
+    public override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let dataSource = collectionView?.dataSource as? DataSource {
+            dataSource.registerReusableViews(collectionView: collectionView!)
+            dataSource.setNeedsLoadContent()
+        }
+    }
+    
+    public override var collectionView: UICollectionView? {
+        get { return super.collectionView }
+        set {
+            let oldCollectionView = collectionView
+            
+            // Always call super, because we don't know EXACTLY what UICollectionViewController does in -setCollectionView:.
+            super.collectionView = newValue
+            
+            oldCollectionView?.removeObserver(self, forKeyPath: "dataSource", context: &dataSourceContext)
+            newValue?.addObserver(self, forKeyPath: "dataSource", options: NSKeyValueObservingOptions.Initial | NSKeyValueObservingOptions.New, context: &dataSourceContext)
+        }
+    }
+    
+    public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if context == &dataSourceContext {
+            let collectionView = object as UICollectionView
+            if let dataSource = collectionView.dataSource as? DataSource {
+                if dataSource.container == nil {
+                    dataSource.container = self
+                }
+            }
+        } else {
+            // For change contexts that aren't the data source, pass them to super.
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+        }
+    }
+    
+    // MARK: DataSourceContainer
+    
+    public let isObscuredByPlaceholder: Bool = false
+    
+    public func dataSourceWillPerform(dataSource: DataSource, sectionAction: SectionAction) {
+        let gridLayout = collectionView?.collectionViewLayout as? GridLayout
+        
+        if let layout = gridLayout {
+            layout.dataSourceWillPerform(dataSource, sectionAction: sectionAction)
+        }
+        
+        if updateDebugging {
+            debugPrintln(sectionAction)
+        }
+        
+        switch (sectionAction, gridLayout) {
+        case (.Insert(let indexSet, _), _):
+            collectionView?.insertSections(indexSet)
+        case (.Remove(let indexSet, _), _):
+            collectionView?.deleteSections(indexSet)
+        case (.Reload(let indexSet), _):
+            collectionView?.reloadSections(indexSet)
+        case (.Move(let from, let to, _), _):
+            collectionView?.moveSection(from, toSection: to)
+        case (.ReloadGlobal, .None):
+            collectionView?.reloadData()
+        default: break
+        }
+    }
+    
+    public func dataSourceWillPerform(dataSource: DataSource, itemAction: ItemAction) {
+        if updateDebugging {
+            debugPrintln(itemAction)
+        }
+        
+        switch itemAction {
+        case .Insert(let indexPaths):
+            collectionView?.insertItemsAtIndexPaths(indexPaths)
+        case .Remove(let indexPaths):
+            collectionView?.deleteItemsAtIndexPaths(indexPaths)
+        case .Reload(let indexPaths):
+            collectionView?.reloadItemsAtIndexPaths(indexPaths)
+        case .Move(let from, let to):
+            collectionView?.moveItemAtIndexPath(from, toIndexPath: to)
+        case .ReloadAll:
+            collectionView?.reloadData()
+        case .BatchUpdate(let update, let completion):
+            collectionView?.performBatchUpdates(update, completion: completion)
+        case .WillLoad, .DidLoad:
+            break
+        }
+    }
+    
+}
--- AdvancedCollectionView/DataSourcePresenter.swift
+++ AdvancedCollectionView/DataSourcePresenter.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
+private func string(fromIndexSet indexSet: NSIndexSet) -> String {
+    return join(", ", lazy(indexSet).map(toString))
+}
+
+private func string(fromIndexPath indexPath: NSIndexPath) -> String {
+    let str = join(", ", lazy(indexPath).map(toString))
+    return "{\(str)}"
+}
+
+private func string(fromIndexPaths indexPaths: [NSIndexPath]) -> String {
+    return join(", ", lazy(indexPaths).map {
+        return string(fromIndexPath: $0)
+    })
+}
+
 public enum SectionAction {
     
     case Insert(NSIndexSet, direction: SectionOperationDirection)
@@ -18,7 +33,26 @@ public enum SectionAction {
     
 }
 
-public protocol DataSourcePresenter: class {
+extension SectionAction: DebugPrintable {
+    
+    public var debugDescription: String {
+        switch self {
+        case .Insert(let indexSet, _):
+            return "INSERT SECTIONS: \(string(fromIndexSet: indexSet))"
+        case .Remove(let indexSet, _):
+            return "DELETE SECTIONS: \(string(fromIndexSet: indexSet))"
+        case .Reload(let indexSet):
+            return "REFRESH SECTIONS: \(string(fromIndexSet: indexSet))"
+        case .Move(let from, let to, _):
+            return "MOVE SECTION: \(from) TO: \(to)"
+        case .ReloadGlobal:
+            return "RELOAD GLOBAL SECTION"
+        }
+    }
+    
+}
+
+public protocol DataSourcePresenter: NSObjectProtocol {
     
     func dataSourceWillPerform(dataSource: DataSource, sectionAction: SectionAction)
     
@@ -35,6 +69,34 @@ public enum ItemAction {
     case BatchUpdate(update: () -> (), completion: ((Bool) -> ())?)
     case WillLoad
     case DidLoad(NSError?)
+    
+}
+
+extension ItemAction: DebugPrintable {
+    
+    public var debugDescription: String {
+        switch self {
+        case .Insert(let indexPaths):
+            return "INSERT ITEMS: \(string(fromIndexPaths: indexPaths))"
+        case .Remove(let indexPaths):
+            return "REMOVE ITEMS: \(string(fromIndexPaths: indexPaths))"
+        case .Reload(let indexPaths):
+            return "REFRESH ITEMS: \(string(fromIndexPaths: indexPaths))"
+        case .Move(let from, let to):
+            return "MOVE ITEM: \(string(fromIndexPath: from)) TO: \(string(fromIndexPath: to))"
+        case .ReloadAll:
+            return "RELOAD"
+        case .BatchUpdate(_, let completion):
+            return "BATCH UPDATE (completion: \(completion != nil))"
+        case .WillLoad:
+            return "WILL LOAD"
+        case .DidLoad(let error):
+            if let error = error {
+                return "LOADED WITH ERROR: \(error)"
+            }
+            return "LOADED"
+        }
+    }
     
 }
 
