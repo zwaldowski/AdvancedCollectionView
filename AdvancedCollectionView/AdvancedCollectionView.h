e3d89b49cc1b85c1712bc8be70f7da003b4e04ea
Zachary Waldowski <zach@waldowski.me>
2015-01-08 20:32:15 -0500

    Remove ObjC collection view implementation

--- AdvancedCollectionView/AdvancedCollectionView.h
+++ AdvancedCollectionView/AdvancedCollectionView.h
@@ -17,31 +17,6 @@ FOUNDATION_EXPORT double AdvancedCollectionViewVersionNumber;
 //! Project version string for AdvancedCollectionView.
 FOUNDATION_EXPORT const unsigned char AdvancedCollectionViewVersionString[];
 
-#import <AdvancedCollectionView/AAPLStateMachine.h>
-#import <AdvancedCollectionView/AAPLContentLoading.h>
-
-#import <AdvancedCollectionView/AAPLDataSource.h>
-#import <AdvancedCollectionView/AAPLBasicDataSource.h>
-#import <AdvancedCollectionView/AAPLComposedDataSource.h>
-#import <AdvancedCollectionView/AAPLSegmentedDataSource.h>
-
-#import <AdvancedCollectionView/AAPLCollectionViewDataSourceGridLayout.h>
-#import <AdvancedCollectionView/AAPLCollectionViewGridLayout.h>
-#import <AdvancedCollectionView/AAPLCollectionViewGridLayoutAttributes.h>
-#import <AdvancedCollectionView/AAPLLayoutMetrics.h>
-
-#import <AdvancedCollectionView/AAPLCollectionViewController.h>
-#import <AdvancedCollectionView/AAPLCollectionViewCell.h>
-#import <AdvancedCollectionView/AAPLBasicCell.h>
-#import <AdvancedCollectionView/AAPLHairlineView.h>
-#import <AdvancedCollectionView/AAPLPinnableHeaderView.h>
-#import <AdvancedCollectionView/AAPLPlaceholderView.h>
-#import <AdvancedCollectionView/AAPLSectionHeaderView.h>
-#import <AdvancedCollectionView/AAPLSegmentedHeaderView.h>
-#import <AdvancedCollectionView/AAPLGridLayoutColorView.h>
-
-#import <AdvancedCollectionView/AAPLDataSource+Headers.h>
-#import <AdvancedCollectionView/UICollectionView+Helpers.h>
-#import <AdvancedCollectionView/UIView+Helpers.h>
+#import <AdvancedCollectionView/AdvancedCollectionView-Swift.h>
 
 #pragma clang diagnostic pop
--- AdvancedCollectionViewTests/AdvancedCollectionViewTests.m
+++ AdvancedCollectionViewTests/AdvancedCollectionViewTests.m
@@ -17,6 +17,8 @@
 
 - (void)setUp {
     [super setUp];
+    
+    GridLayout *foo;
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
