698444f007aa5298c3581126e6973a6cdc547e63
Zachary Waldowski <zach@waldowski.me>
2015-02-10 03:23:52 -0500

    Swift 1.2: NSIndexSet is now natively a sequence

--- AdvancedCollectionView/Foundation.swift
+++ AdvancedCollectionView/Foundation.swift
@@ -52,30 +52,6 @@ extension NSIndexPath {
 
 // MARK: NSIndexSet
 
-extension NSIndexSet: SequenceType {
-    
-    public func generate() -> IndexSetGenerator {
-        return IndexSetGenerator(indexSet: self)
-    }
-    
-    public func range(range: Range<Int>) -> IndexSetGenerator {
-        return IndexSetGenerator(indexSet: self, range: range)
-    }
-    
-    public func reverse(inRange range: Range<Int>? = nil) -> IndexSetGenerator {
-        return IndexSetGenerator(indexSet: self, reverse: true, range: range)
-    }
-    
-    public func map(transform: Int -> Int) -> NSIndexSet {
-        let indexSet = NSMutableIndexSet()
-        for idx in self {
-            indexSet.addIndex(transform(idx))
-        }
-        return indexSet
-    }
-    
-}
-
 extension NSIndexSet {
     
     public convenience init(range: Range<Int>) {
@@ -92,6 +68,14 @@ extension NSIndexSet {
             set.addIndex(idx)
         }
         self.init(indexSet: set)
+    }
+    
+    public func map(transform: Int -> Int) -> NSIndexSet {
+        let indexSet = NSMutableIndexSet()
+        for idx in self {
+            indexSet.addIndex(transform(idx))
+        }
+        return indexSet
     }
     
     var stringValue: String {
@@ -127,74 +111,4 @@ public func +(left: NSIndexSet, right: NSIndexSet) -> NSMutableIndexSet {
     let indexSet = left.mutableCopy() as! NSMutableIndexSet
     indexSet.addIndexes(right)
     return indexSet
-}
-
-// MARK: IndexSetGenerator
-
-public struct IndexSetGenerator: GeneratorType, SequenceType {
-    
-    private let indexSet: NSIndexSet
-    private let reverse: Bool
-    private let range: Range<Int>?
-    private var currentIndex: Int?
-    
-    private func toOptional(value: Int, transform: (Int -> Int?)? = nil) -> Int? {
-        if value == NSNotFound {
-            return nil
-        }
-        
-        if let transform = transform {
-            return transform(value)
-        }
-        
-        return value
-    }
-    
-    private init(indexSet: NSIndexSet, reverse: Bool = false, range: Range<Int>? = nil) {
-        self.indexSet = indexSet
-        self.reverse = reverse
-        self.range = range
-        
-        switch (reverse, range) {
-        case (false, .None):
-            currentIndex = toOptional(indexSet.firstIndex)
-        case (false, .Some(let range)):
-            currentIndex = indexSet.indexGreaterThanOrEqualToIndex(range.startIndex)
-        case (true, .None):
-            currentIndex = toOptional(indexSet.lastIndex)
-        case (true, .Some(let range)):
-            currentIndex = indexSet.indexLessThanIndex(range.endIndex)
-        default:
-            currentIndex = nil
-        }
-    }
-    
-    public mutating func next() -> Int? {
-        if let current = currentIndex {
-            switch (reverse, range) {
-            case (false, .None):
-                currentIndex = toOptional(indexSet.indexGreaterThanIndex(current))
-            case (false, .Some(let range)):
-                currentIndex = toOptional(indexSet.indexGreaterThanIndex(current)) {
-                    $0 < range.endIndex ? $0 : nil
-                }
-            case (true, .None):
-                currentIndex = toOptional(indexSet.indexLessThanIndex(current))
-            case (true, .Some(let range)):
-                currentIndex = toOptional(indexSet.indexLessThanIndex(current)) {
-                    $0 >= range.startIndex ? $0 : nil
-                }
-            default:
-                currentIndex = nil
-            }
-            
-            return current
-        }
-        return nil
-    }
-    
-    public func generate() -> IndexSetGenerator {
-        return IndexSetGenerator(indexSet: indexSet, reverse: reverse, range: range)
-    }
-    
 }
