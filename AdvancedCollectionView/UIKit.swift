78cdb343bf80cbff23ab338c0dbe862d1ef6aa43
Zachary Waldowski <zach@waldowski.me>
2015-03-23 00:40:47 -0400

    w.i.p. some constraint conveniences

--- AdvancedCollectionView/UIKit.swift
+++ AdvancedCollectionView/UIKit.swift
@@ -45,3 +45,38 @@ extension UIView {
     }
     
 }
+
+// MARK: Constraint conveniences
+
+public extension NSLayoutConstraint {
+    
+    convenience init(from item1: UIView, attribute attribute1: NSLayoutAttribute, by relation: NSLayoutRelation = .Equal, to item2: UIView? = nil, attribute attribute2: NSLayoutAttribute = .NotAnAttribute, times multiplier: CGFloat = 1, plus constant: CGFloat = 0, at priority: UILayoutPriority = 1000) {
+        self.init(item: item1, attribute: attribute1, relatedBy: relation, toItem: item2, attribute: attribute2, multiplier: multiplier, constant: constant)
+        self.priority = priority
+    }
+
+}
+
+public func makeConstraints<View: UIView>(#format: String, options: NSLayoutFormatOptions = nil, metrics: [String: CGFloat]? = nil, views: [String: View]) -> [NSLayoutConstraint] {
+    return NSLayoutConstraint.constraintsWithVisualFormat(format, options: options, metrics: metrics, views: views) as! [NSLayoutConstraint]
+}
+
+public func makeConstraints<View: UIView>(#format: String, options: NSLayoutFormatOptions = nil, #metrics: [String: Int], views: [String: View]) -> [NSLayoutConstraint] {
+    return NSLayoutConstraint.constraintsWithVisualFormat(format, options: options, metrics: metrics, views: views) as! [NSLayoutConstraint]
+}
+
+public func appendConstraints<Constraints: ExtensibleCollectionType, View: UIView where Constraints.Generator.Element == NSLayoutConstraint>(inout constraints: Constraints, #format: String, options: NSLayoutFormatOptions = nil, metrics: [String: CGFloat]? = nil, views: [String: View]) {
+    let newConstraints: [NSLayoutConstraint] = NSLayoutConstraint.constraintsWithVisualFormat(format, options: options, metrics: metrics, views: views) as! [NSLayoutConstraint]
+    constraints.extend(newConstraints)
+}
+
+public func appendConstraints<Constraints: ExtensibleCollectionType, View: UIView where Constraints.Generator.Element == NSLayoutConstraint>(inout constraints: Constraints, #format: String, options: NSLayoutFormatOptions = nil, #metrics: [String: Int], views: [String: View]) {
+    let newConstraints: [NSLayoutConstraint] = NSLayoutConstraint.constraintsWithVisualFormat(format, options: options, metrics: metrics, views: views) as! [NSLayoutConstraint]
+    constraints.extend(newConstraints)
+}
+
+public func appendConstraint<Constraints: ExtensibleCollectionType where Constraints.Generator.Element == NSLayoutConstraint>(inout constraints: Constraints, from item1: UIView, attribute attribute1: NSLayoutAttribute, by relation: NSLayoutRelation = .Equal, to item2: UIView? = nil, attribute attribute2: NSLayoutAttribute = .NotAnAttribute, times multiplier: CGFloat = 1, plus constant: CGFloat = 0, at priority: UILayoutPriority = 1000) {
+    let newConstraint = NSLayoutConstraint(item: item1, attribute: attribute1, relatedBy: relation, toItem: item2, attribute: attribute2, multiplier: multiplier, constant: constant)
+    newConstraint.priority = priority
+    constraints.append(newConstraint)
+}
